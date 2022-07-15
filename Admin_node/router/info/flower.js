93c7a011d3656be84a8d849797d3ca2c4ee3cc24
unknown <sunbrian@foxmail.com>
2022-07-15 17:04:52 +0800

    bug fixed

--- Admin_node/router/info/flower.js
+++ Admin_node/router/info/flower.js
@@ -29,7 +29,7 @@ flower.post('/all', async (ctx) => {
 //添加花花
 flower.post('/add', async (ctx) => {
     let flower_info = ctx.request.body;
-    let sql = `INSERT INTO flower (name,florescence,department,allegory,color) VALUES ('${flower_info.name}','${flower_info.florescence}','${flower_info.department}','${flower_info.allegory}','${flower_info.color}')`
+    let sql = `INSERT INTO flower (name,florescence,department,allegory,color,fid) VALUES ('${flower_info.name}','${flower_info.florescence}','${flower_info.department}','${flower_info.allegory}','${flower_info.color}','${flower_info.fid}')`
     let result = await new Promise((resolve) => {
         db.query(sql, (err, data) => {
             if (err) throw err;
@@ -117,7 +117,7 @@ flower.post('/deletePhoto', async (ctx) => {
 //添加花花图片,使用链接上传
 flower.post('/addPhoto', async (ctx) => {
     let flower_info = ctx.request.body;
-    let sql = `INSERT INTO picture (fid,store) VALUES (${flower_info.fid},'${flower_info.store}')`
+    let sql = `INSERT INTO picture (fid,store,name) VALUES (${flower_info.fid},'${flower_info.store}','${flower_info.name}')`
     let result = await new Promise((resolve) => {
         db.query(sql, (err, data) => {
             if (err) throw err;
