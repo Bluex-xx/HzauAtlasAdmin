28a1175186d9206e0aed1610e49559f487dd4e02
unknown <sunbrian@foxmail.com>
2022-07-15 18:28:07 +0800

    1.1.0 version

--- Admin_node/utils/db.js
+++ Admin_node/utils/db.js
@@ -1,10 +1,10 @@
 let mysql = require('mysql')
 let pool = mysql.createPool({
-    host: '81.70.207.239', 
-    port: 3306, 
-    database: 'atlas', 
-    user: 'atlas', 
-    password: 'atlas123' 
+    host: '127.0.0.1', //数据库地址
+    port: 3306, //端口号
+    database: 'demo', //数据库名称
+    user: 'demo', //数据库用户名
+    password: 'demo' //数据库密码
 })
 function query(sql,callback){
     pool.getConnection(function(err,connection){
@@ -15,4 +15,3 @@ function query(sql,callback){
     })
 }
 exports.query = query;
-
